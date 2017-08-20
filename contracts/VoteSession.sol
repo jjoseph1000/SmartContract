d318fcd711a7acc8348092feca6f6b921666d198
Job Joseph <job_joseph@hotmail.com>
2017-08-20 14:43:08 -0500

    Saved contracts.

--- contracts/VoteSession.sol
+++ contracts/VoteSession.sol
@@ -55,14 +55,31 @@ contract VoteSession {
         return _questions.length;
     }
 
-    function getQuestionByIndex(uint questionIndex) returns (bytes32 question) {
-        return _questions[questionIndex];
+    function getQuestionByIndex(uint questionIndex) returns (string question) {
+        return bytes32ToString(_questions[questionIndex]);
     }
 
     function stringToBytes32(string memory source) returns (bytes32 result) {
         assembly {
             result := mload(add(source, 32))
         }
+    }
+
+    function bytes32ToString(bytes32 x) constant returns (string) {
+        bytes memory bytesString = new bytes(32);
+        uint charCount = 0;
+        for (uint j = 0; j < 32; j++) {
+            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+            if (char != 0) {
+                bytesString[charCount] = char;
+                charCount++;
+            }
+        }
+        bytes memory bytesStringTrimmed = new bytes(charCount);
+        for (j = 0; j < charCount; j++) {
+            bytesStringTrimmed[j] = bytesString[j];
+        }
+        return string(bytesStringTrimmed);
     }
 
 }
