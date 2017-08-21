e21eefd682f519d57e208b6ee39e469476a2685e
Job Joseph <job_joseph@hotmail.com>
2017-08-20 23:42:10 -0500

    Saved latest contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
 var VoteToken = artifacts.require("./VoteToken.sol");
 var VoteSession = artifacts.require("./VoteSession.sol");
+var VotingToken = artifacts.require("./VotingToken.sol");
 module.exports = function(deployer) {
   deployer.deploy(VoteToken);  
   deployer.deploy(VoteSession);
+  deployer.deploy(VotingToken);
 };
