2f34ef7d175e8d5b0267f66030d4636000c3e222
Job Joseph <job_joseph@hotmail.com>
2018-03-20 08:11:28 -0500

    honeypot smart contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,9 +2,7 @@ var VoteToken = artifacts.require("./VoteToken.sol");
 var VoteSession = artifacts.require("./VoteSession.sol");
 var VoteSessionBroadRidge = artifacts.require("./VoteSessionBroadRidge.sol");
 var VotingToken = artifacts.require("./VotingToken.sol");
+var ScoreStore = artifacts.require("./ScoreStore.sol");
 module.exports = function(deployer) {
-  deployer.deploy(VoteToken);  
-  deployer.deploy(VoteSession);
-  deployer.deploy(VotingToken);
-  deployer.deploy(VoteSessionBroadRidge);
+  deployer.deploy(ScoreStore);
 };
--- truffle.js
+++ truffle.js
@@ -4,11 +4,6 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
-    },
-    production: {
-      host: "bclviuadx.southcentralus.cloudapp.azure.com",
-      port: 8545,
-      network_id: "*" // Match any network id
     }
   }
 };
